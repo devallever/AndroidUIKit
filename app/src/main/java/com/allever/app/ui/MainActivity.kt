bff3cff65c01aebf8c22f6c3adc565f3eecb93ea
devallever <devallever@163.com>
2020-02-24 11:06:40 +0800

    init

---
+++ app/src/main/java/com/allever/app/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.allever.app.ui
+
+import android.os.Bundle
+import com.allever.lib.common.app.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Android UI 依赖库
+
+> 依赖AndroidDependencyLib项目的common模块
+
+
+## CircleImageView
+圆形ImageView
+
+
+## AutoLayout
+自动换行布局
+
+## SearchView
+封装EditText的搜索栏
